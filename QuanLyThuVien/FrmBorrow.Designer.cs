6fe37b4edabc2400da33bac8a1187aca27944369
Nguyễn Lê Văn Dũng <131602164+VanDung-dev@users.noreply.github.com>
2026-04-19 15:00:19 +0700

    feat: Cập nhật logic mượn sách và định danh lại các điều khiển trong FrmBorrow
    - Cập nhật `FrmBorrow.Designer.cs` bằng cách đổi tên các biến điều khiển giao diện (UI controls) thành các tên có ý nghĩa như `btnSearchReader`, `dtpBorrowDate`, `txtReaderID`, `dgvBooks`, `btnBorrow` và `btnCancel`.
    - Thêm phương thức `LoadBooksFromDB` trong `FrmBorrow.cs` để tải danh sách sách "Sẵn sàng" từ cơ sở dữ liệu vào `dgvBooks`.
    - Thêm xử lý sự kiện cho nút Tìm kiếm (`BtnSearchReader_Click`) để kiểm tra thông tin độc giả từ cơ sở dữ liệu.
    - Thêm xử lý sự kiện cho nút Mượn (`BtnBorrow_Click`) để thực hiện tạo phiếu mượn (`PhieuMuon`), chi tiết mượn (`ChiTietMuon`) và cập nhật tình trạng sách.
    - Thêm xử lý sự kiện đóng biểu mẫu khi nhấn nút Hủy (`BtnCancel_Click`).

--- QuanLyThuVien/FrmBorrow.Designer.cs
+++ QuanLyThuVien/FrmBorrow.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace QuanLyThuVien
+namespace QuanLyThuVien
 {
     partial class FrmBorrow
     {
@@ -29,24 +29,24 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnSearchReader = new System.Windows.Forms.Button();
+            this.dtpBorrowDate = new System.Windows.Forms.DateTimePicker();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtReaderID = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.dgvBooks = new System.Windows.Forms.DataGridView();
+            this.btnBorrow = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBooks)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.btnSearchReader);
+            this.groupBox1.Controls.Add(this.dtpBorrowDate);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.txtReaderID);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(16, 15);
@@ -58,23 +58,23 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Chi tiết thông tin";
             // 
-            // button1
+            // btnSearchReader
             // 
-            this.button1.Location = new System.Drawing.Point(80, 192);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(160, 42);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Tìm kiếm";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnSearchReader.Location = new System.Drawing.Point(80, 192);
+            this.btnSearchReader.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSearchReader.Name = "btnSearchReader";
+            this.btnSearchReader.Size = new System.Drawing.Size(160, 42);
+            this.btnSearchReader.TabIndex = 4;
+            this.btnSearchReader.Text = "Tìm kiếm";
+            this.btnSearchReader.UseVisualStyleBackColor = true;
             // 
-            // dateTimePicker1
+            // dtpBorrowDate
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(13, 132);
-            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(305, 32);
-            this.dateTimePicker1.TabIndex = 3;
+            this.dtpBorrowDate.Location = new System.Drawing.Point(13, 132);
+            this.dtpBorrowDate.Margin = new System.Windows.Forms.Padding(4);
+            this.dtpBorrowDate.Name = "dtpBorrowDate";
+            this.dtpBorrowDate.Size = new System.Drawing.Size(305, 32);
+            this.dtpBorrowDate.TabIndex = 3;
             // 
             // label2
             // 
@@ -86,13 +86,13 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "Ngày mượn";
             // 
-            // textBox1
+            // txtReaderID
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 55);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(305, 32);
-            this.textBox1.TabIndex = 1;
+            this.txtReaderID.Location = new System.Drawing.Point(13, 55);
+            this.txtReaderID.Margin = new System.Windows.Forms.Padding(4);
+            this.txtReaderID.Name = "txtReaderID";
+            this.txtReaderID.Size = new System.Drawing.Size(305, 32);
+            this.txtReaderID.TabIndex = 1;
             // 
             // label1
             // 
@@ -104,53 +104,53 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Mã đọc giả";
             // 
-            // dataGridView1
+            // dgvBooks
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(372, 15);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(724, 494);
-            this.dataGridView1.TabIndex = 1;
+            this.dgvBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBooks.Location = new System.Drawing.Point(372, 15);
+            this.dgvBooks.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvBooks.Name = "dgvBooks";
+            this.dgvBooks.RowHeadersWidth = 51;
+            this.dgvBooks.Size = new System.Drawing.Size(724, 494);
+            this.dgvBooks.TabIndex = 1;
             // 
-            // button2
+            // btnBorrow
             // 
-            this.button2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(963, 516);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(133, 37);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Mượn";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnBorrow.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBorrow.Location = new System.Drawing.Point(963, 516);
+            this.btnBorrow.Margin = new System.Windows.Forms.Padding(4);
+            this.btnBorrow.Name = "btnBorrow";
+            this.btnBorrow.Size = new System.Drawing.Size(133, 37);
+            this.btnBorrow.TabIndex = 2;
+            this.btnBorrow.Text = "Mượn";
+            this.btnBorrow.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnCancel
             // 
-            this.button3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(821, 516);
-            this.button3.Margin = new System.Windows.Forms.Padding(4);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(133, 37);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Hủy";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnCancel.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(821, 516);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(133, 37);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Hủy";
+            this.btnCancel.UseVisualStyleBackColor = true;
             // 
             // FrmBorrow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1112, 567);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnBorrow);
+            this.Controls.Add(this.dgvBooks);
             this.Controls.Add(this.groupBox1);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FrmBorrow";
             this.Text = "Phiếu Mượn Sách";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBooks)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -158,13 +158,13 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtReaderID;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DateTimePicker dtpBorrowDate;
+        private System.Windows.Forms.Button btnSearchReader;
+        private System.Windows.Forms.DataGridView dgvBooks;
+        private System.Windows.Forms.Button btnBorrow;
+        private System.Windows.Forms.Button btnCancel;
     }
 }
--- QuanLyThuVien/FrmBorrow.cs
+++ QuanLyThuVien/FrmBorrow.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using QuanLyThuVien.Models;
+using QuanLyThuVien.Helpers;
+using System.Data.SqlClient;
 
 namespace QuanLyThuVien
 {
@@ -15,6 +18,117 @@ namespace QuanLyThuVien
         public FrmBorrow()
         {
             InitializeComponent();
+            
+            this.btnSearchReader.Click += BtnSearchReader_Click;
+            this.btnBorrow.Click += BtnBorrow_Click;
+            this.btnCancel.Click += BtnCancel_Click;
+            
+            LoadBooksFromDB();
+        }
+
+        private void LoadBooksFromDB()
+        {
+            string query = "SELECT IDSach, TenSach, TacGia, TinhTrang FROM ThongTinSach WHERE TinhTrang = N'Sẵn sàng'";
+            try
+            {
+                dgvBooks.DataSource = DatabaseHelper.ExecuteQuery(query);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi tải danh sách sách: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void BtnSearchReader_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtReaderID.Text))
+            {
+                MessageBox.Show("Vui lòng nhập mã độc giả!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string query = "SELECT HoTen FROM TheDocGia WHERE IDDocGia = @ID";
+            SqlParameter[] parameters = { new SqlParameter("@ID", txtReaderID.Text) };
+            
+            try
+            {
+                object result = DatabaseHelper.ExecuteScalar(query, parameters);
+                if (result != null)
+                {
+                    MessageBox.Show($"Tìm thấy độc giả: {result} (Mã: {txtReaderID.Text})", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Không tìm thấy độc giả!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi tìm kiếm độc giả: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void BtnBorrow_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtReaderID.Text))
+            {
+                MessageBox.Show("Vui lòng chọn độc giả!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (dgvBooks.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Vui lòng chọn ít nhất một quyển sách!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string phieuMuonID = "PM" + DateTime.Now.ToString("yyyyMMddHHmmss").Substring(8);
+            
+            try
+            {
+                // 1. Insert into PhieuMuon
+                string queryPhieu = "INSERT INTO PhieuMuon (IDPhieuMuon, IDNguoiMuon) VALUES (@ID, @ReaderID)";
+                SqlParameter[] paramsPhieu = {
+                    new SqlParameter("@ID", phieuMuonID),
+                    new SqlParameter("@ReaderID", txtReaderID.Text)
+                };
+                DatabaseHelper.ExecuteNonQuery(queryPhieu, paramsPhieu);
+
+                // 2. Insert into ChiTietMuon for each selected book
+                foreach (DataGridViewRow row in dgvBooks.SelectedRows)
+                {
+                    string sachID = row.Cells["IDSach"].Value.ToString();
+                    string ctID = "CT" + DateTime.Now.Ticks.ToString().Substring(10);
+                    
+                    string queryCT = "INSERT INTO ChiTietMuon (IDChiTietMuon, IDPhieuMuon, IDSach, NgayMuon, HanTra, TinhTrangTra) " +
+                                     "VALUES (@CTID, @PMID, @SachID, @NgayMuon, @HanTra, N'Đang mượn')";
+                    
+                    SqlParameter[] paramsCT = {
+                        new SqlParameter("@CTID", ctID),
+                        new SqlParameter("@PMID", phieuMuonID),
+                        new SqlParameter("@SachID", sachID),
+                        new SqlParameter("@NgayMuon", dtpBorrowDate.Value),
+                        new SqlParameter("@HanTra", dtpBorrowDate.Value.AddDays(14)),
+                    };
+                    DatabaseHelper.ExecuteNonQuery(queryCT, paramsCT);
+
+                    // Update Book status
+                    string queryUpdateBook = "UPDATE ThongTinSach SET TinhTrang = N'Đang mượn' WHERE IDSach = @SachID";
+                    DatabaseHelper.ExecuteNonQuery(queryUpdateBook, new SqlParameter[] { new SqlParameter("@SachID", sachID) });
+                }
+
+                MessageBox.Show($"Lập phiếu mượn thành công!\nMã phiếu: {phieuMuonID}", "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi lập phiếu mượn: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
